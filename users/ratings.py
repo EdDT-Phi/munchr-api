16b84cd73cd9e6df801b973ac25686284d6043ee
Eddie <eddt.phi@gmail.com>
2017-03-30 00:27:35 -0500

    activities

--- users/ratings.py
+++ users/ratings.py
@@ -17,33 +17,53 @@ def user_rating():
 
 	return jsonify(result={'success': True})
 
-@ratings_blueprint.route('/users/activity/<int:user_id>')
-def get_activity(user_id):
-	ratings = utils.select_query(queries.get_activity)
+@ratings_blueprint.route('/users/activity/friends/<int:user_id>')
+def get_friends_activity(user_id):
+	ratings = utils.select_query(queries.get_friends_activity)
 	results = []
 	utils.add_rows_to_list(ratings, results, ('first_name', 'last_name', 'photo', 'liked', 'res_name', 'review_date', 'user_id'))
 
 	for i in range(len(results)):
-		ago = datetime.now() - results[i]['review_date']
-		if ago.days > 1:
-			results[i]['review_date'] = '%d days ago' % ago.days
-		elif ago.days == 1:
-			results[i]['review_date'] = 'Yesterday'
-		elif ago.seconds >= 60*60:
-			hrs = ago.seconds // (60*60)
-			sing = 'hour'
-			if hrs > 1:
-				sing += 's'
-			results[i]['review_date'] = '%d %s ago' % (hrs, sing)
-		elif ago.seconds > 60:
-			results[i]['review_date'] = 'A few minutes ago'
-		else:
-			results[i]['review_date'] = 'A few seconds ago'
+		results[i]['review_date'] = time_to_text(datetime.now() - results[i]['review_date'])
 
-		if user_id == results[i]['user_id']:
-			results[i]['first_name'] = 'You'
-			results[i]['last_name'] = ''
+		# if user_id == results[i]['user_id']:
+		# 	results[i]['first_name'] = 'You'
+		# 	results[i]['last_name'] = ''
 
-		del results[i]['user_id']
+		# del results[i]['user_id']
 
 	return jsonify(results=results)
+
+@ratings_blueprint.route('/users/activity/<int:user_id>')
+def get_activity(user_id):
+	ratings = utils.select_query(queries.get_activity, (user_id,))
+	results = []
+	utils.add_rows_to_list(ratings, results, ('first_name', 'last_name', 'photo', 'liked', 'res_name', 'review_date'))
+
+	for i in range(len(results)):
+		results[i]['review_date'] = time_to_text(datetime.now() - results[i]['review_date'])
+		
+
+		# if user_id == results[i]['user_id']:
+		# 	results[i]['first_name'] = 'You'
+		# 	results[i]['last_name'] = ''
+
+		# del results[i]['user_id']
+
+	return jsonify(results=results)
+
+def time_to_text(ago):
+	if ago.days > 1:
+		return '%d days ago' % ago.days
+	elif ago.days == 1:
+		return 'Yesterday'
+	elif ago.seconds >= 60*60:
+		hrs = ago.seconds // (60*60)
+		sing = 'hour'
+		if hrs > 1:
+			sing += 's'
+		return '%d %s ago' % (hrs, sing)
+	elif ago.seconds > 60:
+		return 'A few minutes ago'
+	else:
+		return 'A few seconds ago'	
